35fcd68d70a6ac163debe5866dac2117ae70b9ad
Gregory Casamento <greg.casamento@gmail.com>
2019-12-16 13:23:46 -0500

    Add all initial files.

---
+++ NSFontCollection_test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSFontCollection_test
+//
+//  Created by Gregory Casamento on 12/15/19.
+//  Copyright © 2019 Open Logic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSFontCollection_test/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  NSFontCollection_test
+//
+//  Created by Gregory Casamento on 12/15/19.
+//  Copyright © 2019 Open Logic. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
